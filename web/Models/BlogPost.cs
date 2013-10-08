7bbf412ab3f620b727f3a6983cd2875bfe8d716f
Phil Campbell <philoushka@gmail.com>
2013-10-08 05:18:47 -0700

    remove and sort usings. tweak post count on front to be 16

--- web/Models/BlogPost.cs
+++ web/Models/BlogPost.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
 
 namespace Thyme.Web.Models
 {
@@ -11,23 +6,16 @@ namespace Thyme.Web.Models
     {
         public DateTime CreatedOn { get; set; }
         public DateTime? PublishedOn { get; set; }
-
         public string Title { get; set; }
-
         public string Body { get; set; }
-
         public string UrlSlug { get; set; }
-
         public string Intro { get; set; }
-
     }
 
     public class BlogPostMetaProperties
     {
         public string Title { get; set; }
         public string PublishedOn { get; set; }
-
         public string Intro { get; set; }
     }
-
 }
