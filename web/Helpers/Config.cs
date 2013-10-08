1ddd80b145b67d1534db224abe716f5956992ed2
Phil Campbell <philoushka@gmail.com>
2013-10-07 21:03:39 -0700

    Summary comments on both properties. Change exception return value in NumPosts; int.Max instead of zero.

--- web/Helpers/Config.cs
+++ web/Helpers/Config.cs
@@ -2,31 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
 
 namespace Thyme.Web.Helpers
 {
     public static class Config
     {
+
+
+        /// <summary>
+        /// Gets the number of posts to show on the front page of the blog site. If any exception occurs, 
+        /// a large number will be returned (int.maxvalue) and allow basically all posts to be shown.
+        /// </summary>
         public static int NumPostsFrontPage
         {
             get
             {
                 try
                 {
-                    int numPosts = int.Parse(System.Configuration.ConfigurationManager.AppSettings["FrontPageShowNumberPosts"].ToString());
-                    return (numPosts>0)?numPosts:int.MaxValue;
+                    int numPosts = int.Parse(ConfigurationManager.AppSettings["FrontPageShowNumberPosts"].ToString());
+                    return (numPosts > 0) ? numPosts : int.MaxValue;
                 }
-                catch (Exception) { return 0; }
+                catch (Exception) { return int.MaxValue; }
             }
         }
 
+        /// <summary>
+        /// Gets the datetime format from config. If config setting is missing, or any exception, the default MMM dd YYYY format will be returned.
+        /// </summary>
         public static string DateTimeFormat
         {
-            get {
-                  try
-                {return System.Configuration.ConfigurationManager.AppSettings["DateTimeFormat"].ToString();
+            get
+            {
+                try
+                {
+                    return ConfigurationManager.AppSettings["DateTimeFormat"].ToString();
                 }
-                  catch (Exception) { return "MMM dd YYYY"; }
+                catch (Exception) { return "MMM dd YYYY"; }
             }
         }
     }
