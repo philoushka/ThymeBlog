d31cc033cdfacbb47689a741c8a6d4a3e0997cd0
Phil Campbell <philoushka@gmail.com>
2015-04-10 13:24:48 -0700

    keylime changes round 1

--- web/Models/BlogPost.cs
+++ web/Models/BlogPost.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Thyme.Web.Models
 {
     public class BlogPost
     {
-        
+        public BlogPost()
+        {
+            Tags = Enumerable.Empty<string>();
+        }
         public DateTime? PublishedOn { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
@@ -13,6 +18,7 @@ namespace Thyme.Web.Models
         public string FileName { get; set; }
         public string SHA { get; set; }
         public string Url { get; set; }
+        public IEnumerable<string> Tags { get; set; }
     }
 
     public class BlogPostMetaProperties
@@ -20,5 +26,7 @@ namespace Thyme.Web.Models
         public string Title { get; set; }
         public string PublishedOn { get; set; }
         public string Intro { get; set; }
+        public IEnumerable<string> Tags { get; set; }
+
     }
 }
--- web/ViewModels/BlogPost_vm.cs
+++ web/ViewModels/BlogPost_vm.cs
@@ -5,5 +5,9 @@ namespace Thyme.Web.ViewModels
     public class BlogPost_vm
     {
         public BlogPost BlogPost { get; set; }
+        public string WriteTags()
+        {
+            return string.Join(", ", BlogPost.Tags);
+        }
     }
 }
